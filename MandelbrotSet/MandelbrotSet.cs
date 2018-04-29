8cdaa6e1771d0d2066b2a0088f03e8d459e01315
joham97 <joham97@web.de>
2018-04-29 08:39:42 +0200

    Initial Commit

---
+++ MandelbrotSet/MandelbrotSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelbrotSet
+{
+    class MandelbrotSet
+    {
+        public uint[] Calculate(int offsetX, int offset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MandelbrotSet/MandelbrotSetRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelbrotSet
+{
+    class MandelbrotSetRenderer
+    {
+
+        private Texture2D canvas;
+        private Rectangle tracedSize;
+        private UInt32[] pixels;
+        private MandelbrotSet mandelbrotSet;
+
+        public void Initialize(GraphicsDeviceManager graphics)
+        {
+            tracedSize = graphics.GraphicsDevice.PresentationParameters.Bounds;
+            canvas = new Texture2D(graphics.GraphicsDevice, tracedSize.Width, tracedSize.Height, false, SurfaceFormat.Color);
+            pixels = new UInt32[tracedSize.Width * tracedSize.Height];
+
+            mandelbrotSet = new MandelbrotSet();
+            mandelbrotSet.Calculate();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            canvas.SetData<UInt32>(pixels, 0, tracedSize.Width * tracedSize.Height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(canvas, new Rectangle(0, 0, tracedSize.Width, tracedSize.Height), Color.White);
+            spriteBatch.End();
+
+        }
+
+    }
+}
